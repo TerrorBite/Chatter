10187ce8c3d7935a8f3d9f054dbf201fbdbf7d4d
Dral <m.c.dral@gmail.com>
2012-03-09 20:38:10 +0100

    Tatara, set autocrlf to true, that will fix any linux/mac/windows line ending problems: http://help.github.com/line-endings/

--- src/main/java/com/dral/Chatter/Chatter.java
+++ src/main/java/com/dral/Chatter/Chatter.java
@@ -19,6 +19,7 @@ package com.dral.Chatter;
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+ 
 
 import com.dral.Chatter.configuration.ChatterConfigThing;
 import com.dral.Chatter.configuration.Configuration;
